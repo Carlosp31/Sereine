84a552eca3f0c96b7591f0b252f727d96921fa42
unknown <cpardobar@gmail.com>
2026-01-24 12:09:07 -0500

    mi cambio

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 // Servir archivos estáticos (HTML, CSS, JS, imágenes)
 app.use(express.static(path.join(__dirname, "..", "public")));
